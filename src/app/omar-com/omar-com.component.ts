99b379d0f687c3eafa3dcbef3a15464d75bf7f5e
omalk98 <omarmohammad1951@gmail.com>
2022-03-10 10:32:00 -0500

    2 files modified

--- src/app/omar-com/omar-com.component.ts
+++ src/app/omar-com/omar-com.component.ts
@@ -6,6 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./omar-com.component.css'],
 })
 export class OmarComComponent implements OnInit {
+  myPhoto: string =
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.xcitefun.net%2Fusers%2F2014%2F07%2F361487%2Cxcitefun-nature-color-5.jpg&f=1&nofb=1';
   @Input('email') email: string;
   @Input('phone') phone: string;
   constructor() {}
